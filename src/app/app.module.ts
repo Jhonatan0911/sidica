6bd5c38ce9be7730d3f8453cff60f90a28157fc1
Jhonatan0911 <jpinalozano911@gmail.com>
2022-02-21 01:08:22 -0500

    body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { HomeComponent } from './pages/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -9,7 +9,21 @@ export class NavComponent implements OnInit {
 
   constructor() { }
 
+  quieto: boolean = true;
+  menu: any;
+  a: any
   ngOnInit(): void {
+    var y:any;
+    window.onscroll = function() {
+      y = window.scrollY;
+      console.log(y);
+    };
+    setInterval(() => {
+      if(y > 100){
+        this.quieto = false;
+      }else{
+        this.quieto = true;
+      }
+    }, 500);
   }
-
 }
